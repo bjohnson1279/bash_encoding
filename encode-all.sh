107792ceefaf453e1e88f8d88f86909bbfaeacff
Brent Johnson <johnsonbrentp@gmail.com>
2022-04-18 14:46:06 -0600

    Adding scripts for copying from a shared recordings folder on the network and encoding all recording files in a local folder

---
+++ encode-all.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+clear 
+
+# Enter location of local folder where Plex recordings are stored
+RECORDING_PATH=""
+
+# Enter location where encode files will be stored
+DESTINATION_PATH=""
+
+# FFMPEG encoder to be used
+ENC_TYPE="libx264"
+
+# Video filters applied, default yadif for deinterlacing
+VF="yadif"
+
+# Speed of encoding process, default veryslow to preserve disk space
+PRESET="veryslow"
+
+# Quality level, default 22
+QUALITY=22
+
+# Delete original file after encoding is complete? Set to 1 for YES
+DEL_ORIG=1
+
+# Function to obtain length of video
+getDuration() {
+    ffmpeg -i "${1}" 2>&1 | grep "Duration" | cut -d ' ' -f 4 | sed s/,// | sed s/00://
+}
+
+if [[ -d "${RECORDING_PATH}" ]]
+then
+    cd "${RECORDING_PATH}" || continue
+    file_count=`ls -A 2>/dev/null | wc -l`
+    if [ $file_count != 0 ]
+    then
+        # Iterate through all directories in folder containing your recordings
+        for dir in */; do
+            echo "${dir}"
+
+            # Validate directory exists, in case folder was deleted after list was obtained
+            if [[ -d "${dir}" ]]; then
+                cd "${dir}" || continue
+
+                # Get number of folders in directory (Seasons)
+                dir_file_count=`ls -A 2>/dev/null | wc -l`
+                echo "${dir} directory file count: ${dir_file_count}"
+
+                if [ $dir_file_count != 0 ]; then
+                    # Iterate through Season folders
+                    for season in */; do
+                        echo "${season}"
+                        if [[ -d "${season}" ]]; then
+                            cd "${season}" || continue
+                            
+                            # Get number of .ts files found in Season directory
+                            ts_dir_file_count=`ls -A *.ts 2>/dev/null | wc -l`
+                            echo "${season} ts file count: ${ts_dir_file_count}"
+
+                            if [ $ts_dir_file_count != 0 ]; then
+                                for i in *.ts; do
+                                    echo "${i}"
+                                    # Get video duration of encoding source
+                                    src_duration=$(getDuration "${i}")
+                                    src_duration="${src_duration%.*}"
+
+                                    # Apply pattern matching to remove extraneous data in file name
+                                    # Removes year, dashes, and adds space between season and episode number
+                                    shopt -s extglob
+                                    new_file=${i//\(*\) /}}
+                                    new_file=${new_file//- /}}
+                                    new_file=${new_file/E/ E}
+                                    new_file=${new_file%.*}
+                                    echo "New File: ${new_file}"
+                                    new_file_full="${DESTINATION_PATH}${new_file}.mp4"
+
+                                    # Validate existience of file
+                                    if [[ -f "${i}" ]]; then
+                                        # Skip if encoded file already exists, encode if not
+                                        if [[ ! -f "${new_file_full}" ]]; then
+                                            # Check for optional video filter
+                                            if [[ $VF != "" ]]; then
+                                                ffmpeg -i "${i}" -vf $VF -c:v $ENC_TYPE -c:a copy -preset $PRESET -crf $QUALITY "${new_file_full}"
+                                            else
+                                                ffmpeg -i "${i}" -c:v $ENC_TYPE -c:a copy -preset $PRESET -crf $QUALITY "${new_file_full}"
+                                            fi
+                                        fi
+
+                                        # OPTIONAL: Delete source (ts) file when new file (mp4) is created, for space saving purposes
+                                        # Set DEL_ORIG value to 0 above if you don't want this to happen
+                                        if [[ $DEL_ORIG == 1 ]]; then
+                                            dest_duration=$(getDuration "${new_file_full}")
+                                            dest_duration="${dest_duration%.*}"
+                                            echo "dest_duration: ${dest_duration}"
+
+                                            echo "Source File Duration: ${src_duration}"
+                                            echo "Destination File Duration: ${dest_duration}"
+
+                                            if [[ "${src_duration}" == "${dest_duration}" ]]; then
+                                                rm "${i}"
+                                            fi
+                                        fi
+                                    fi
+                                    shopt -u extglob
+                                done # END for loop for ts files in directory
+                            fi
+
+                            # Done with directory, go one up to move on to next
+                            cd ..
+                        fi
+                    done # END for loop for Season directories
+                fi
+
+                # Go back to recording path to move on to the next show
+                cd "${RECORDING_PATH}"
+            fi
+        done # END for loop for all TV show directories
+    fi
+fi
---
+++ network-copy.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Copy Plex recordings from a network location
+
+clear
+
+# Enter your mount path for network share
+MNT_SHARE_PATH=""
+
+# Enter mount location on your local machine
+LOCAL_SHARE_PATH=""
+
+# Enter local path where your recordings will be stored
+RECORDING_PATH=""
+
+# Required Available Space in MB, default is 1 GB
+REQUIRED_DISK_AMOUNT=1000
+
+getAvailMB() {
+    df -h /mnt/d -BM --output=avail | sed /Avail/d
+}
+
+folderSize() {
+    # $1 folder path
+    du -sh "${1}" -BM
+}
+
+# Getting available disk space
+AVAIL=$(getAvailMB)
+AVAIL_MB=${AVAIL//[!0-9]/}
+
+# Output available disk space if you'd like, comment out if not
+echo ${AVAIL}
+
+if [[ AVAIL_MB -lt $REQUIRED_DISK_AMOUNT ]]; then
+    echo "Insufficient disk space to copy recordings"
+    exit
+fi
+
+folderSync() {
+    # $1 => Mounted source of folder to copy
+    # $2 => Default size in MB of required available space to copy
+
+    # Check if directory exists
+    if [[ -d "${1}" ]]; then
+        # Get space available on local hard drive
+        AVAIL=$(getAvailMB)
+        AVAIL_MB=${AVAIL//[!0-9]/}
+
+        if [[ AVAIL_MB -lt $2 ]]; then
+            echo "Insufficient disk space to copy recordings from ${1}"
+        else
+            echo "Copying from ${1}"
+            FOLDER_SIZE=`du -sh "${1}" -BM`
+            FOLDER_SIZE_MB=${FOLDER_SIZE//[!0-9]/}
+            echo "Actual Folder Size in MB: ${FOLDER_SIZE_MB}"
+            if [[ AVAIL_MB -lt $FOLDER_SIZE_MB ]]; then
+                echo "Insufficient disk space to copy recordings from ${1}"
+            else
+                rsync -avzh --progress "${1}" "${RECORDING_PATH}"
+            fi
+        fi
+    else
+        echo "Directory ${1} currently not available"
+    fi
+}
+
+# Enter mounted location of folder to copy
+# Example: If I am copying recordings of Seinfeld from a network location, enter the mounted location as follows
+# SEINFELD_SRC="/mnt/sambashare/Seinfeld (1989)"
+COPY_FOLDER_SRC=""
+
+# Enter amount of space in MB required to copy to local folder
+# Example: Enter the typical size of the network folder when recordings have just completed
+#   If the Seinfeld recordings folder is typically around 2.5G after completion, enter that amount in MB below
+# SEINFELD_REQ=2500
+COPY_FOLDER_REQ=2500
+
+# Mount Network Location
+# Mac users should be able to comment out if network location already mounted
+if [[ ! -d "${COPY_FOLDER_SRC}" ]]
+then
+    sudo mount -t drvfs "${MNT_SHARE_PATH}" $LOCAL_SHARE_PATH
+fi
+
+# Call the folderSync() function, passing the folder location and default MB required to copy to your local hard drive
+folderSync COPY_FOLDER_SRC COPY_FOLDER_REQ
+
+# For additional folders to copy, repeat the call to folderSync() function with location and required disk space for each folder to copy
