750d4f1e7a62ec76be91ee00271d11658dee2ded
Brent Johnson <37778321+bjohnson1279@users.noreply.github.com>
2024-04-30 16:51:36 -0600

    Update encode-m4v.sh
    For loop to re-encode all .m4v files in a directory

--- encode-m4v.sh
+++ encode-m4v.sh
@@ -1,1 +1,2 @@
 # Encode all .m4v files in a directory 
+for i in *.m4v; do ffmpeg -i "$i" -vf yadif -c:v libx264 -preset veryslow -pix_fmt yuv420p -crf 21 -movflags faststart -y "${i%.*}.mp4"; done
